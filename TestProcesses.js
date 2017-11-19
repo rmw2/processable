c7447c19546c1652fd787df8f2b69ca5d076b219
Rob Whitaker <rmw2@princeton.edu>
2017-11-19 15:41:29 -0500

    Added example fibonacci process

---
+++ TestProcesses.js
@@ -0,0 +1,24 @@
+const { Process } = require('./Process.js');
+const x86 = require('./x86.js');
+
+const fibonacci = {
+	text: [
+		['pushl', 	'$1'],
+		['pushl', 	'$1'],
+		['movq', 	'$0', '%eax'],
+		['addl', 	'$1', '%eax'],
+		['movl', 	'4(%rsp)','%ebx'],
+		['movl', 	'(%rsp)','%ecx'],
+		['addl', 	'%ecx', '%ebx'],
+		['pushl', 	'%ebx'],
+		['jmp',		'loop'],
+	],
+
+	labels: {
+		loop: 0x03 
+	},
+};
+
+module.exports = { fibonacci };
+
+let p = new Process(fibonacci.text, fibonacci.labels, x86.Chip);
