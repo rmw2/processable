8140e710c2d0453b892ec64cb44d771e0413c81e
Rob Whitaker <rmw2@princeton.edu>
2017-11-25 17:12:48 -0500

    reorganized again, ejecting from start-react-app

--- docs/miniproc/miniproc-ng.js
+++ docs/miniproc/miniproc-ng.js
@@ -1,6 +1,6 @@
 window.angular || document.write('<script src="lib/angular.min.js">\x3C/script>');
 
-var vm = angular.module('vm', [])
+var vm = angular.module('miniproc', [])
 	.controller('processCtrl', function ($scope, $interval) {
 		$scope.P = new Process([
 			'push $1',
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.jsx?$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+    ]
+  }
+}
