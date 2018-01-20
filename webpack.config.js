c683954ebe9210e307fb3f64575cd85a772dc263
Rob Whitaker <rmw2@princeton.edu>
2018-01-19 17:37:41 -0700

    updating webpack to target /docs/alpha

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   entry: './src/App.jsx',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve('docs/alpha'),
     filename: 'bundle.js'
   },
   module: {
