31fe4d596f624763f598d0eaa8e2555a5df996d1
Rob Whitaker <rmw2@princeton.edu>
2018-01-19 17:24:53 -0700

    renaming some directories for gh-pages

---
+++ interact.js
@@ -0,0 +1,46 @@
+/**
+ * animations and other non-angular interactions
+ */
+
+$(document).ready(function () {
+	hide(0);
+	show(500);
+
+	window.history.replaceState({}, 'processable [prototype]', '/Processable/prototype');
+});
+
+function hide(speed) {
+	$('#registers').animate({
+		top: '-=100%'
+	}, speed);
+
+	$('#stack').animate({
+		right: '-=100%'
+	}, speed);
+
+	$('#controls').animate({
+		bottom: '-=100%'
+	}, speed);
+
+	$('#text').animate({
+		left: '-=100%'
+	}, speed);
+}
+
+function show(speed) {
+	$('#registers').animate({
+		top: '+=100%'
+	}, speed);
+
+	$('#stack').animate({
+		right: '+=100%'
+	}, speed);
+
+	$('#controls').animate({
+		bottom: '+=100%'
+	}, speed);
+
+	$('#text').animate({
+		left: '+=100%'
+	}, speed);
+}
