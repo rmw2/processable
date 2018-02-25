c85eb13b6e82d4e0de6f6c4f646a720d522cb320
Rob Whitaker <rmw2@princeton.edu>
2018-02-25 11:00:01 -0500

    Added new multiple-listener events interface to Signals

--- src/vm/Signals.js
+++ src/vm/Signals.js
@@ -11,6 +11,10 @@ const SIGNALS = [
     'SIGALRM'
 ];
 
+/**
+ * Signal-Callback abstraction with at most one handler installed
+ * per signal
+ */
 export default class Signals {
     constructor(signals=SIGNALS) {
         this.signals = {};
@@ -26,8 +30,42 @@ export default class Signals {
         this.signals[name] = () => null;
     }
 
-    dispatch(signal, args) {
-        // Call that signal handler yo
-        this.signals[signal](args);
+    dispatch(signal, ...args) {
+        if (signal in this.signals)
+            this.signals[signal](...args);
+    }
+}
+
+/**
+ * Event-Callbacks abstraction with potentially multiple callbacks registered
+ * per event
+ */
+export class Events {
+    constructor() {
+        this.events = {};
+    }
+
+    register(name, callback) {
+        if (!(name in this.events))
+            this.events[name] = [];
+
+        this.events[name].push(callback);
+    }
+
+    unregsiter(name, callback) {
+        if (!(name in this.events))
+            return;
+
+        for (const i in this.events[name])
+            if (this.events[name][i] === callback)
+                this.events[name].splice(i, 1);
+    }
+
+    dispatch(name, args) {
+        if (!(name in this.events))
+            return;
+
+        for (const i in this.events[name])
+            this.events[name](...args);
     }
 }
